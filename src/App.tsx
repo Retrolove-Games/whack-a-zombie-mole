a121f8925d5542bde2bdd404056a0438f0d20b1d
Konrad Fedorczyk <fedek6@gmail.com>
2021-01-31 18:46:58 +0100

    Use global styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React, { useReducer } from 'react';
 import { initialState, reducer } from './GameState';
 import { TestButton } from './components/TestButton';
 import { GameCtx } from './context/GameContext';
+import { GlobalStyles } from './GlobalStyles';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div className="App">
+      <GlobalStyles />
       <GameCtx.Provider value={{state, dispatch}}>
         <TestButton />
       </GameCtx.Provider>

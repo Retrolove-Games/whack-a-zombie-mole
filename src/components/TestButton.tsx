76fcbfeb22b3f5250b0848a556262711c2c9f854
Konrad Fedorczyk <fedek6@gmail.com>
2021-01-31 18:57:45 +0100

    Add test for global CSS vars

--- src/components/TestButton.tsx
+++ src/components/TestButton.tsx
@@ -1,14 +1,19 @@
 import React, { useContext } from "react";
 import { GameCtx } from "../context/GameContext";
+import styled from 'styled-components/macro';
+
+const Wrapper = styled.div`
+  background-color: var(--color-background);
+`;
 
 export const TestButton = () => {
-  const { dispatch, state } = useContext(GameCtx);
+  const { dispatch } = useContext(GameCtx);
 
   return (
-    <>
+    <Wrapper>
       <button onClick={() => dispatch({ type: 'CHANGE_SCREEN', screen: 'menu' })}>Change screen!</button>
       <button onClick={() => dispatch({ type: 'INCREMENT_POINTS', points: 10 })}>Add points!</button>
       <input type="text" onChange={(e) => dispatch({ type: 'SET_NICKNAME', nickname: e.target.value })}></input>
-    </>
+    </Wrapper>
   );
 };

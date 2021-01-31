8fbe0bae1172aa21cb4faee8e17a53846c65fccd
Konrad Fedorczyk <fedek6@gmail.com>
2021-01-31 18:44:46 +0100

    Add global style

---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  html {
+    --color-text: white;
+    --color-background: white;
+    --color-primary: rebeccapurple;
+  }
+`;

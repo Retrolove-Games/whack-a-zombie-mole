af0346fdbb7a91cb266770637d415d1dadca41b7
Konrad Fedorczyk <fedek6@gmail.com>
2021-01-31 18:57:22 +0100

    Use styled-reset in global styles

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -1,9 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 export const GlobalStyles = createGlobalStyle`
   html {
+    ${reset}
     --color-text: white;
-    --color-background: white;
+    --color-background: black;
     --color-primary: rebeccapurple;
   }
 `;

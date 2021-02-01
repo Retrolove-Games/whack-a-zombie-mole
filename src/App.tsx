704f6c50d104d69aa0ef322f8a977ad49a29a1ca
Konrad Fedorczyk <fedek6@gmail.com>
2021-02-01 08:21:47 +0100

    Add wrapper to app

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,21 @@ import { initialState, reducer } from './GameState';
 import { TestButton } from './components/TestButton';
 import { GameCtx } from './context/GameContext';
 import { GlobalStyles } from './GlobalStyles';
+import styled from 'styled-components/macro';
+
+// Atari 2600 emulation wrapper
+const Wrapper = styled.div`
+  width: var(--native-width);
+  height: var(--native-height);
+  background-color: var(--color-background);
+  color: var(--color-text);
+`;
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <div className="App">
+    <Wrapper className="App">
       <GlobalStyles />
       <GameCtx.Provider value={{state, dispatch}}>
         <TestButton />
@@ -16,7 +25,7 @@ function App() {
       <div>{state.screen}</div>
       <div>{state.points}</div>
       <div>{state.nickname}</div>
-    </div>
+    </Wrapper>
   );
 }
 

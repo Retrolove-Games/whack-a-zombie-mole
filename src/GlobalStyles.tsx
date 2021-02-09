0501a87124f24ca2eaeae6910e732c3c8449fc5c
Konrad Fedorczyk <fedek6@gmail.com>
2021-02-09 10:38:51 +0100

    Import working Atari font

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -1,9 +1,25 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import AtariClassicChunky from './assets/fonts/AtariClassicChunky.woff';
+import AtariClassicChunky2 from './assets/fonts/AtariClassicChunky.woff2';
 
-export const GlobalStyles = createGlobalStyle`
+export default createGlobalStyle`
+  @font-face {
+    font-family: 'Atari Classic Chunky';
+    src: url(${AtariClassicChunky2}) format('woff2'),
+        url(${AtariClassicChunky}) format('woff');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+  }
+
   html {
     ${reset}
+    font-family: 'Atari Classic Chunky';
+    font-size: 10px;
+    text-rendering: optimizeSpeed;
+
+    // Vars
     --color-text: white;
     --color-background: black;
     --color-primary: rebeccapurple;
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { GlobalStyles } from './GlobalStyles';
+import GlobalStyles from './GlobalStyles';
 import { ScreenWrapper } from './components/ScreenWrapper';
 
 ReactDOM.render(

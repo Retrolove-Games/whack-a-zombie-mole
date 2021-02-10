1b794661f5094f8a77ad8b47a6c923666fd4453b
Konrad Fedorczyk <fedek6@gmail.com>
2021-02-10 23:38:16 +0100

    Add menu animation and buttons mockup

--- src/views/Menu.tsx
+++ src/views/Menu.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
+import styled from 'styled-components/macro';
+import {useSpring, animated} from 'react-spring';
+import * as easings from 'd3-ease';
 import { Image } from '../components/Image';
 import Title from '../assets/title-logo.png';
 
+const Wrapper = styled.div`
+  width: var(--native-width);
+  height: var(--native-height);
+  text-align: center;
+  padding-top: 20px;
+  position: relative;
+  overflow: hidden
+`;
+
+const MenuContainer = styled.ul`
+  list-style: none;
+  padding: 0 0;
+  margin: 15px 0 0 0;
+
+  li {
+    margin-bottom: 10px;
+  }
+`;
+
 export const Menu = () => {
+
+  const logoAnimation = useSpring({
+    top: '0',
+    from: { top: '-140px' },
+    config: { easing: easings.easeBounceOut, duration: 1200 },
+  });
+
   return (
-    <div>
-      <Image src={Title} alt="" width="200" height="104" />
-      <p>Start Game</p>
-    </div>
+    <Wrapper>
+      <animated.div style={{...logoAnimation, position: 'relative'}}>
+        <Image src={Title} alt="" width="200" height="104" />
+      </animated.div>
+      <MenuContainer>
+        <li>Start Game</li>
+        <li>Enable sound</li>
+        <li>Highscores</li>
+      </MenuContainer>
+    </Wrapper>
   );
 }

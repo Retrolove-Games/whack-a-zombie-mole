e0608295552fe489e70e2aec076ecce80428f0e6
Konrad Fedorczyk <contact@realhe.ro>
2021-04-03 20:09:21 +0200

    Add click to restart info

--- src/views/Highscores.tsx
+++ src/views/Highscores.tsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 import styled from "styled-components/macro";
+import { BlinkInfo } from "../components/BlinkInfo";
 import { WrapperBase } from "../styledComponents";
+import { SfxCtx } from "../context/SfxContext";
 import { GameCtx } from "../context/GameContext";
 import { getScores, scoreInterface } from "../api/Api";
 
@@ -12,23 +14,35 @@ const Wrapper = styled(WrapperBase)`
 `;
 
 export const Highscores = () => {
+  const { dispatch, state } = useContext(GameCtx);
+  const { playSfx } = useContext(SfxCtx);
   const [highscores, setHighscores] = useState<Array<scoreInterface>>([]);
 
   useEffect(() => {
-    getScores("whack-a-zombie-mole").then(response => {
+    getScores("whack-a-zombie-mole").then((response) => {
       if (response.ok) {
-        response.json().then(data => {
+        response.json().then((data) => {
           setHighscores(data as Array<scoreInterface>);
         });
       }
-    })
+    });
   }, []);
 
   return (
-    <Wrapper>
+    <Wrapper
+      onClick={() => {
+        dispatch({ type: "CHANGE_SCREEN", screen: "menu" });
+        playSfx("start");
+      }}
+    >
       {highscores.map((score, index) => (
-        <div key={index}>{score.nickname} {score.score}</div>
+        <div key={index}>
+          {score.nickname} {score.score}
+        </div>
       ))}
+      <div className="footer">
+        <BlinkInfo>CLICK TO RESTART</BlinkInfo>
+      </div>
     </Wrapper>
   );
-}
+};

ce9bcb5800b2618367e7860c62285f2c912a1c31
Konrad Fedorczyk <contact@realhe.ro>
2021-04-04 12:21:18 +0200

    Add example config file

---
+++ src/config/example-config.ts
@@ -0,0 +1,14 @@
+export default {
+  gameElements: 12,
+  initialGameSpeed: 900,
+  gameSpeedup: 20,
+  speedupFactor: 5,
+  maximumSpeed: 600,
+  timeLimit: 90,
+  badClickPoints: 10,
+  goodClickPoints: 10,
+  maxComboFactor: 4,
+  apiUrl: "[score api url]",
+  encryptionKey: "[encryption key]",
+  maxNickLenght: 15,
+};
